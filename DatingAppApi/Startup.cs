9e0f719735499d2882a9466470f95e12723c4022
Nikos Thomadakis <Nikos Thomadakis>
2021-11-24 00:38:23 +0200

    Final changes before publishing

--- DatingAppApi/Startup.cs
+++ DatingAppApi/Startup.cs
@@ -64,11 +64,15 @@ namespace DatingAppApi
 
             app.UseAuthorization();
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<PresenceHub>("hubs/presence");
                 endpoints.MapHub<MessageHub>("hubs/message");
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
